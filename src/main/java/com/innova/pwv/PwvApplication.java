ceb99e62f6c1e7560a1573981c4c2446714aa525
lintsai <lintsai@crm.com.tw>
2023-07-04 18:14:34 +0800

    Initial commit

---
+++ src/main/java/com/innova/pwv/PwvApplication.java
@@ -0,0 +1,13 @@
+package com.innova.pwv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PwvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innova/pwv/PwvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.pwv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
